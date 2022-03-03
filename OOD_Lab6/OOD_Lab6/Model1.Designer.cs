5fe58b63fba2cad2103b23c01e9c1b5dba779632
Steven Kelly <stevenkelly418@gmail.com>
2022-03-03 11:12:54 +0000

    => Added Data Model for creation of Database.

---
+++ OOD_Lab6/OOD_Lab6/Model1.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'C:\Users\User\My Drive\Computer Science Studies\Year2\Subjects\Semester2\ObjectOrientedDevelopment\Labs\Lab6\OOD_Lab6\OOD_Lab6\Model1.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
